f42403b68bb539b187cee0aa29cc17b8aa098b34
Ronald Rink <ronald.rink@d-fens.net>
2015-10-28 11:44:18 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# net.sharedop.msp.entityTypes.ManagedOperatingSystem
+# TOSCA CSAR ManagedOperatingSystem
+
 net.sharedop.msp.entityTypes.ManagedOperatingSystem
+
+TOSCA Cloud Service Archive (CSAR) for product Managed Operating System
---
+++ definitions/Created-Continue.cs
@@ -0,0 +1,43 @@
+/**
+ * Copyright 2015 d-fens GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Contracts;
+
+namespace net.sharedop.msp.entityTypes.ManagedOperatingSystem
+{
+    public class CreatedContinue : BaseEntity
+    {
+        public new CreatedContinue SetDefaults()
+        {
+            base.SetDefaults();
+
+            Description = "optional-default-description";
+            SomeNiftyParameter = 42;
+
+            return this;
+        }
+
+        [Required]
+        public string Name { get; set; }
+        [StringLength(250)]
+        [DisplayName("This is a description")]
+        public string Description { get; set; }
+        public int SomeNiftyParameter { get; set; }
+    }
+}
